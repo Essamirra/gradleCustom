983218064db9165c03a4d00039ad5b46f3a40ee8
essamirra <maria.pronina@outlook.com>
2021-09-11 21:23:42 +0300

    second step for plugin

--- buildSrc/src/main/java/chordpro/ChordProPlugin.java
+++ buildSrc/src/main/java/chordpro/ChordProPlugin.java
@@ -7,5 +7,12 @@ public class ChordProPlugin implements Plugin<Project> {
     @Override
     public void apply(Project project) {
         System.out.println("Hello from chord pro plugin");
+
+        // https://docs.gradle.org/current/userguide/base_plugin.html
+        project.getPlugins().apply("base");
+
+        project.getTasks().register("generateChordProPdfs", GenerateChordProPdfsTask.class);
+
+        project.getTasks().named("assemble").configure(task -> task.dependsOn("generateChordProPdfs"));
     }
 }
