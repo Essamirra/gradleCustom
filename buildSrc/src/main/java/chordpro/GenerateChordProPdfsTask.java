0fcac07d847e92ec40d20c5ce7bf495db4b56a8b
essamirra <maria.pronina@outlook.com>
2021-09-12 20:25:04 +0300

    incremental

--- buildSrc/src/main/java/chordpro/GenerateChordProPdfsTask.java
+++ buildSrc/src/main/java/chordpro/GenerateChordProPdfsTask.java
@@ -2,7 +2,12 @@ package chordpro;
 
 import org.gradle.api.DefaultTask;
 import org.gradle.api.file.FileCollection;
+import org.gradle.api.file.FileType;
 import org.gradle.api.tasks.*;
+import org.gradle.work.ChangeType;
+import org.gradle.work.FileChange;
+import org.gradle.work.Incremental;
+import org.gradle.work.InputChanges;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -21,6 +26,7 @@ public abstract class GenerateChordProPdfsTask extends DefaultTask {
 
     @InputFiles
     @PathSensitive(PathSensitivity.NAME_ONLY)
+    @Incremental
     abstract FileCollection getSources();
     abstract void setSources(FileCollection sources);
 
@@ -29,18 +35,25 @@ public abstract class GenerateChordProPdfsTask extends DefaultTask {
     abstract void setOutputDir(File outputDir);
 
     @TaskAction
-    void buildChord() throws Exception {
-        for (File choFile : getSources()) {
-            File pdfFile = new File(getOutputDir(), removeExtension(choFile.getName()) + ".pdf");
-            getProject().exec(execSpec -> {
-                try {
-                    execSpec.setStandardOutput(new FileOutputStream(pdfFile));
-                    execSpec.setExecutable("chordpro");
-                    execSpec.setArgs(Arrays.asList(choFile.getAbsolutePath(), "--config=" + getConfig()));
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
+    void buildChord(InputChanges inputChanges) throws Exception {
+        for (FileChange change : inputChanges.getFileChanges(getSources())) {
+            if (change.getFileType() != FileType.DIRECTORY) {
+                File choFile = change.getFile();
+                File pdfFile = new File(getOutputDir(), removeExtension(choFile.getName()) + ".pdf");
+                if (change.getChangeType() == ChangeType.REMOVED) {
+                    pdfFile.delete();
+                } else {
+                    getProject().exec(execSpec -> {
+                        try {
+                            execSpec.setStandardOutput(new FileOutputStream(pdfFile));
+                            execSpec.setExecutable("chordpro");
+                            execSpec.setArgs(Arrays.asList(choFile.getAbsolutePath(), "--config=" + getConfig()));
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                    });
                 }
-            });
+            }
         }
     }
 }
